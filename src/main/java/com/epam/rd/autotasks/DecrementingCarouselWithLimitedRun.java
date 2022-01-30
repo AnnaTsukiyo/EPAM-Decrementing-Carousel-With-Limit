0331e32bb9f61659d16dcb9808ed9f945204a47a
AnnaTsukiyo <annett.sav4enko@gmail.com>
2022-01-31 01:16:54 +0200

    done

--- src/main/java/com/epam/rd/autotasks/DecrementingCarouselWithLimitedRun.java
+++ src/main/java/com/epam/rd/autotasks/DecrementingCarouselWithLimitedRun.java
@@ -1,7 +1,49 @@
 package com.epam.rd.autotasks;
 
-public class DecrementingCarouselWithLimitedRun extends DecrementingCarousel{
+import java.util.Arrays;
+
+public class DecrementingCarouselWithLimitedRun extends DecrementingCarousel {
+
+    private int i = 0;
+    private boolean runEd = false;
+    protected int actionLimit;
+
     public DecrementingCarouselWithLimitedRun(final int capacity, final int actionLimit) {
         super(capacity);
+        DecrementingCarouselWithLimitedRun.this.arr = new int[capacity];
+        this.actionLimit = actionLimit;
+    }
+
+    @Override
+    public int next() {
+        if (arr.length == 0 || Arrays.stream(arr).sum() == 0 || actionLimit == 0)
+            return -1;
+
+        if (i >= capacity)
+            while (arr[i] == 0) {
+                i++;
+                if (i > arr.length - 1)
+                    i = 0;
+            }
+
+        if (arr[i] > 0 && actionLimit > 0)
+            actionLimit--;
+        return arr[i++]--;
+
+    }
+
+    @Override
+    public DecrementingCarouselWithLimitedRun run() {
+        if (!runEd) {
+            runEd = true;
+            return DecrementingCarouselWithLimitedRun.this;
+        } else
+            return null;
+    }
+
+    @Override
+    public boolean isFinished() {
+        return Arrays.stream(arr).sum() == 0 && actionLimit == 0;
     }
 }
+
