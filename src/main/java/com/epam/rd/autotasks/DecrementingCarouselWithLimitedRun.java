ecdab2ff4446102a44045819d1f6c242af37bf8b
AnnaTsukiyo <annett.sav4enko@gmail.com>
2022-01-31 12:26:09 +0200

    amended

--- src/main/java/com/epam/rd/autotasks/DecrementingCarouselWithLimitedRun.java
+++ src/main/java/com/epam/rd/autotasks/DecrementingCarouselWithLimitedRun.java
@@ -19,17 +19,18 @@ public class DecrementingCarouselWithLimitedRun extends DecrementingCarousel {
         if (arr.length == 0 || Arrays.stream(arr).sum() == 0 || actionLimit == 0)
             return -1;
 
-        if (i >= capacity && actionLimit > 0)
+        if (i >= capacity)
             while (arr[i] == 0) {
                 i++;
                 if (i > arr.length - 1)
                     i = 0;
             }
 
-        if (arr[i] > 0 && actionLimit > 0)
+        if (arr[i] > 0 && actionLimit > 0) {
             actionLimit--;
-        return arr[i++]--;
-
+            return arr[i++]--;
+        }
+        return 0;
     }
 
     @Override
@@ -46,4 +47,3 @@ public class DecrementingCarouselWithLimitedRun extends DecrementingCarousel {
         return Arrays.stream(arr).sum() == 0 || actionLimit == 0;
     }
 }
-
